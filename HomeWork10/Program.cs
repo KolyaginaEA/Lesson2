e95642a9161b5052b6f04dc158d96b3f3582839f
Ekaterina <katya_usova@bk.ru>
2023-01-24 22:00:40 +0300

    HomeWork10

---
+++ HomeWork10/Program.cs
@@ -0,0 +1,18 @@
+﻿
+        {
+            System.Console.Write("N = ");
+            int N = System.Convert.ToInt32(System.Console.ReadLine());
+            Second(N);
+            System.Console.ReadKey(true);
+        }
+
+        static bool Second(int N)
+        {
+            if(N < 100 || N > 999)
+            {
+                System.Console.Write("Число не трёхзначное");
+                return false;
+            }
+            System.Console.Write("{0} -> {1}", N, (N - (N / 100) * 100) / 10);
+            return true;
+        }
